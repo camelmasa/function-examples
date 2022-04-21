656198304afc77b34f8bd5475c8fd7594a89b4c2
Di Wen <di.wen@shopify.com>
2022-04-21 15:37:36 -0700

    Add default

---
+++ discounts/rust/order-discount-type/default/README.md
@@ -0,0 +1,65 @@
+### Prerequisites
+
+* [Install Rust](https://www.rust-lang.org/tools/install)
+* Add `wasm32-wasi` target: `rustup target add wasm32-wasi`
+
+### Build
+
+```
+make
+```
+
+### Note
+
+If you have to interact with the auto-generated Rust types from GQL schema,
+
+  * Install [graphql_client_cli](https://github.com/graphql-rust/graphql-client/tree/main/graphql_client_cli) by `cargo install graphql_client_cli`
+  * `graphql-client generate -o build/ ./input.graphql --schema-path ./schema.graphql` (using `input.graphql` as an example)
+  * Check `build.input.rs`
+
+Use SCREAMING_SNAKE_CASE as the conventional enum value. However, the codegen tool (graphql_client)
+will generate the type as `ValueType::FixedAmount`.
+
+```
+enum ValueType {
+  FIXED_AMOUNT
+}
+```
+
+### Demo
+
+configuration
+
+```json
+{"value":"5.0","excluded_variant_gids":[]}
+```
+
+output
+
+```JSON
+{
+  "discounts": [
+    {
+      "targets": [
+        {
+          "targetType": "ORDER_SUBTOTAL",
+          "id": null,
+          "quantity": null,
+          "excludedVariantIds": [
+          ]
+        }
+      ],
+      "value": {
+        "type": "PERCENTAGE",
+        "value": 5.0,
+        "appliesToEachItem": null
+      },
+      "conditions": null,
+      "message": "5% off order subtotal"
+    }
+  ],
+  "discount_application_strategy": "first"
+}
+```
+
+
---
+++ discounts/rust/order-discount-type/default/src/main.rs
@@ -0,0 +1,222 @@
+use serde::{Deserialize, Serialize};
+
+use graphql_client::GraphQLQuery;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "schema.graphql",
+    query_path = "input.graphql",
+    response_derives = "Debug, Clone, PartialEq",
+    normalization = "rust"
+)]
+struct Input;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "schema.graphql",
+    query_path = "output.graphql",
+    response_derives = "Debug, Clone, PartialEq",
+    normalization = "rust"
+)]
+struct Output;
+
+type ID = i64;
+
+#[derive(Clone, Debug, Deserialize)]
+struct Config {
+    value: Option<String>,
+    excluded_variant_gids: Option<Vec<String>>,
+}
+
+impl Config {
+    const DEFAULT_VALUE: f64 = 20.0;
+
+    fn get_value(&self) -> f64 {
+        match &self.value {
+            Some(value) => value.parse().unwrap(),
+            _ => Self::DEFAULT_VALUE,
+        }
+    }
+
+    fn excluded_variant_ids(&self) -> Vec<ID> {
+        match &self.excluded_variant_gids {
+            Some(excluded_variant_gids) => excluded_variant_gids
+                .iter()
+                .map(Self::convert_gid_to_id)
+                .collect(),
+            _ => {
+                vec![]
+            }
+        }
+    }
+
+    fn convert_gid_to_id(gid: &String) -> ID {
+        gid.split('/').last().map(|id| id.parse().unwrap()).unwrap()
+    }
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct Payload {
+    input: input::ResponseData,
+    configuration: Config,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let payload: Payload = serde_json::from_reader(std::io::BufReader::new(std::io::stdin()))?;
+    let mut out = std::io::stdout();
+    let mut serializer = serde_json::Serializer::new(&mut out);
+    script(payload)?.serialize(&mut serializer)?;
+    Ok(())
+}
+
+fn script(payload: Payload) -> Result<output::Variables, Box<dyn std::error::Error>> {
+    let (_input, config) = (payload.input, payload.configuration);
+    // let merchandise_lines = &input.merchandise_lines.unwrap_or_default();
+
+    let targets = vec![output::Target {
+        targetType: output::TargetType::OrderSubtotal,
+        excludedVariantIds: Some(config.excluded_variant_ids()),
+        id: None,
+        quantity: None,
+    }];
+    Ok(output::Variables {
+        discounts: vec![output::Discount {
+            message: Some(format!("{}% off order subtotal", config.get_value())),
+            conditions: None,
+            targets,
+            value: output::Value {
+                type_: output::ValueType::Percentage,
+                value: config.get_value(),
+                appliesToEachItem: None,
+            },
+        }],
+        discount_application_strategy: output::DiscountApplicationStrategy::First,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn payload(config: Option<Config>) -> Payload {
+        let input = r#"
+        {
+            "input": {
+                "merchandiseLines": [
+                    { "variant": { "id": 0 } },
+                    { "variant": { "id": 1 } }
+                ]
+            },
+            "configuration": {
+                "value": null,
+                "excluded_variant_gids": ["gid://shopify/ProductVariant/1", "gid://shopify/ProductVariant/2"]
+            }
+        }
+        "#;
+        let default_payload: Payload = serde_json::from_str(&input).unwrap();
+
+        match config {
+            Some(configuration) => Payload {
+                configuration,
+                ..default_payload
+            },
+            None => default_payload,
+        }
+    }
+
+    #[test]
+    fn test_discount_with_default_value() {
+        let payload = payload(None);
+        let output = serde_json::json!(script(payload).unwrap());
+
+        let expected_json = r#"
+            {
+                "discounts": [{
+                    "message": "20% off order subtotal",
+                    "conditions": null,
+                    "targets": [{
+                        "targetType": "ORDER_SUBTOTAL",
+                        "excludedVariantIds": [1, 2],
+                        "id": null,
+                        "quantity": null
+                    }],
+                    "value": {
+                        "type": "PERCENTAGE",
+                        "value": 20.0,
+                        "appliesToEachItem": null
+                    }
+                }],
+                "discount_application_strategy": "first"
+            }
+        "#;
+
+        let expected_output: serde_json::Value = serde_json::from_str(expected_json).unwrap();
+        assert_eq!(output.to_string(), expected_output.to_string());
+    }
+
+    #[test]
+    fn test_discount_with_value() {
+        let payload = payload(Some(Config {
+            value: Some("10.0".to_string()),
+            excluded_variant_gids: None,
+        }));
+        let output = serde_json::json!(script(payload).unwrap());
+
+        let expected_json = r#"
+            {
+                "discounts": [{
+                    "message": "10% off order subtotal",
+                    "conditions": null,
+                    "targets": [{
+                        "targetType": "ORDER_SUBTOTAL",
+                        "excludedVariantIds": [],
+                        "id": null,
+                        "quantity": null
+                    }],
+                    "value": {
+                        "type": "PERCENTAGE",
+                        "value": 10.0,
+                        "appliesToEachItem": null
+                    }
+                }],
+                "discount_application_strategy": "first"
+            }
+        "#;
+
+        let expected_output: serde_json::Value = serde_json::from_str(expected_json).unwrap();
+        assert_eq!(output.to_string(), expected_output.to_string());
+    }
+
+    #[test]
+    fn test_discount_with_excluded_variant_gids() {
+        let payload = payload(Some(Config {
+            value: None,
+            excluded_variant_gids: Some(vec!["gid://shopify/ProductVariant/0".to_string()]),
+        }));
+        let output = serde_json::json!(script(payload).unwrap());
+
+        let expected_json = r#"
+            {
+                "discounts": [{
+                    "message": "20% off order subtotal",
+                    "conditions": null,
+                    "targets": [{
+                        "targetType": "ORDER_SUBTOTAL",
+                        "excludedVariantIds": [0],
+                        "id": null,
+                        "quantity": null
+                    }],
+                    "value": {
+                        "type": "PERCENTAGE",
+                        "value": 20.0,
+                        "appliesToEachItem": null
+                    }
+                }],
+                "discount_application_strategy": "first"
+            }
+        "#;
+
+        let expected_output: serde_json::Value = serde_json::from_str(expected_json).unwrap();
+        assert_eq!(output.to_string(), expected_output.to_string());
+    }
+}
