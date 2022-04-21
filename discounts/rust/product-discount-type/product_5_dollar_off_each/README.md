28ad6287a40ab37e7460a03658ecac3edabf6132
Di Wen <di.wen@shopify.com>
2022-04-20 18:10:08 -0700

    Add product_5_dollar_off_each demo rust script

---
+++ discounts/rust/product-discount-type/product_5_dollar_off_each/README.md
@@ -0,0 +1,62 @@
+### Prerequisites
+
+* [Install Rust](https://www.rust-lang.org/tools/install)
+* Add `wasm32-wasi` target: `rustup target add wasm32-wasi`
+
+### Build
+
+```
+make
+```
+
+### Note
+
+If you have to interact with the auto-generated Rust types from GQL schema,
+
+  * Install [graphql_client_cli](https://github.com/graphql-rust/graphql-client/tree/main/graphql_client_cli) by `cargo install graphql_client_cli`
+  * `graphql-client generate -o build/ ./input.graphql --schema-path ./schema.graphql` (using `input.graphql` as an example)
+  * Check `build.input.rs`
+
+Use SCREAMING_SNAKE_CASE as the conventional enum value. However, the codegen tool (graphql_client)
+will generate the type as `ValueType::FixedAmount`.
+
+```
+enum ValueType {
+  FIXED_AMOUNT
+}
+```
+
+### Demo output
+
+```JSON
+
+{
+  "discounts": [
+    {
+      "targets": [
+        {
+          "targetType": "product_variant",
+          "id": 2,
+          "quantity": null
+        },
+        {
+          "targetType": "product_variant",
+          "id": 1,
+          "quantity": null
+        }
+      ],
+      "value": {
+        "type": "FIXED_AMOUNT",
+        "value": 5.0,
+        "appliesToEachItem": true
+      },
+      "conditions": null,
+      "message": "$5 off"
+    }
+  ],
+  "discount_application_strategy": "first"
+}
+
+```
+
+
---
+++ discounts/rust/product-discount-type/product_5_dollar_off_each/src/main.rs
@@ -0,0 +1,215 @@
+use serde::{Deserialize, Serialize};
+
+use graphql_client::GraphQLQuery;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "schema.graphql",
+    query_path = "input.graphql",
+    response_derives = "Debug, Clone, PartialEq",
+    normalization = "rust"
+)]
+struct Input;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "schema.graphql",
+    query_path = "output.graphql",
+    response_derives = "Debug, Clone, PartialEq",
+    normalization = "rust"
+)]
+struct Output;
+
+#[derive(Clone, Debug, Deserialize)]
+struct Config {
+    value: Option<String>,
+    excluded_variant_gids: Option<Vec<String>>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct Payload {
+    input: input::ResponseData,
+    configuration: Config,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let payload: Payload = serde_json::from_reader(std::io::BufReader::new(std::io::stdin()))?;
+    let mut out = std::io::stdout();
+    let mut serializer = serde_json::Serializer::new(&mut out);
+    script(payload)?.serialize(&mut serializer)?;
+    Ok(())
+}
+
+fn script(payload: Payload) -> Result<output::Variables, Box<dyn std::error::Error>> {
+    const DEFAULT_VALUE: f64 = 5.0;
+
+    let (input, config) = (payload.input, payload.configuration);
+    let value: f64 = if let Some(value) = config.value {
+        value.parse()?
+    } else {
+        DEFAULT_VALUE
+    };
+    let merchandise_lines = &input.merchandise_lines.unwrap_or_default();
+    let excluded_variant_gids = &config.excluded_variant_gids.unwrap_or_default();
+    let targets = targets(&merchandise_lines, &excluded_variant_gids);
+    return Ok(build_output(value, targets));
+}
+
+fn targets(
+    merchandise_lines: &Vec<input::InputMerchandiseLines>,
+    excluded_variant_gids: &[String],
+) -> Vec<output::ProductVariantTarget> {
+    let excluded_variant_ids: Vec<u64> = excluded_variant_gids
+        .iter()
+        .filter_map(|gid| gid.split('/').last().map(|id| id.parse().unwrap()))
+        .collect();
+
+    merchandise_lines
+        .iter()
+        .filter_map(|line| match line.variant {
+            Some(ref variant) if !excluded_variant_ids.contains(&(variant.id as u64)) => {
+                Some(output::ProductVariantTarget {
+                    targetType: output::TargetType::ProductVariant,
+                    id: variant.id,
+                    quantity: None,
+                })
+            }
+            _ => None,
+        })
+        .collect()
+}
+
+fn build_output(value: f64, targets: Vec<output::ProductVariantTarget>) -> output::Variables {
+    output::Variables {
+        discounts: vec![output::Discount {
+            message: Some(format!("${} off", value)),
+            conditions: None,
+            targets,
+            value: output::Value {
+                type_: output::ValueType::FixedAmount,
+                value,
+                appliesToEachItem: Some(true),
+            },
+        }],
+        discount_application_strategy: output::DiscountApplicationStrategy::First,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn payload(configuration: Config) -> Payload {
+        let input = r#"
+        {
+            "input": {
+                "merchandiseLines": [
+                    { "variant": { "id": 0 } },
+                    { "variant": { "id": 1 } }
+                ]
+            },
+            "configuration": {
+                "value": null,
+                "excluded_variant_gids": null
+            }
+        }
+        "#;
+        let default_payload: Payload = serde_json::from_str(&input).unwrap();
+        Payload {
+            configuration,
+            ..default_payload
+        }
+    }
+
+    #[test]
+    fn test_discount_with_default_value() {
+        let payload = payload(Config {
+            value: None,
+            excluded_variant_gids: None,
+        });
+        let output = serde_json::json!(script(payload).unwrap());
+
+        let expected_json = r#"
+            {
+                "discounts": [{
+                    "message": "$5 off",
+                    "conditions": null,
+                    "targets": [
+                        { "targetType": "product_variant", "id": 0, "quantity": null },
+                        { "targetType": "product_variant", "id": 1, "quantity": null }
+                    ],
+                    "value": {
+                        "type": "FIXED_AMOUNT",
+                        "value": 5.0,
+                        "appliesToEachItem": true
+                    }
+                }],
+                "discount_application_strategy": "first"
+            }
+        "#;
+
+        let expected_output: serde_json::Value = serde_json::from_str(expected_json).unwrap();
+        assert_eq!(output.to_string(), expected_output.to_string());
+    }
+
+    #[test]
+    fn test_discount_with_value() {
+        let payload = payload(Config {
+            value: Some("10".to_string()),
+            excluded_variant_gids: None,
+        });
+        let output = serde_json::json!(script(payload).unwrap());
+
+        let expected_json = r#"
+            {
+                "discounts": [{
+                    "message": "$10 off",
+                    "conditions": null,
+                    "targets": [
+                        { "targetType": "product_variant", "id": 0, "quantity": null },
+                        { "targetType": "product_variant", "id": 1, "quantity": null }
+                    ],
+                    "value": {
+                        "type": "FIXED_AMOUNT",
+                        "value": 10.0,
+                        "appliesToEachItem": true
+                    }
+                }],
+                "discount_application_strategy": "first"
+            }
+        "#;
+
+        let expected_output: serde_json::Value = serde_json::from_str(expected_json).unwrap();
+        assert_eq!(output.to_string(), expected_output.to_string());
+    }
+
+    #[test]
+    fn test_discount_with_excluded_variant_gids() {
+        let payload = payload(Config {
+            value: None,
+            excluded_variant_gids: Some(vec!["gid://shopify/ProductVariant/0".to_string()]),
+        });
+        let output = serde_json::json!(script(payload).unwrap());
+
+        let expected_json = r#"
+            {
+                "discounts": [{
+                    "message": "$5 off",
+                    "conditions": null,
+                    "targets": [
+                        { "targetType": "product_variant", "id": 1, "quantity": null }
+                    ],
+                    "value": {
+                        "type": "FIXED_AMOUNT",
+                        "value": 5.0,
+                        "appliesToEachItem": true
+                    }
+                }],
+                "discount_application_strategy": "first"
+            }
+        "#;
+
+        let expected_output: serde_json::Value = serde_json::from_str(expected_json).unwrap();
+        assert_eq!(output.to_string(), expected_output.to_string());
+    }
+}
