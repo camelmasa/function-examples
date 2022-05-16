7496082203f131708aecf8d9392e65cc2100dd86
Delta Pham <d.pham@shopify.com>
2022-05-16 15:04:06 -0400

    Simplify order discount script

--- discounts/rust/order-discounts/default/src/main.rs
+++ discounts/rust/order-discounts/default/src/main.rs
@@ -8,15 +8,6 @@ type Void = ();
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "schema.graphql",
-    query_path = "input.graphql",
-    response_derives = "Debug, Clone, PartialEq",
-    normalization = "rust"
-)]
-struct Input;
-
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "schema.graphql",
     query_path = "output.graphql",
     response_derives = "Debug, Clone, PartialEq",
     normalization = "rust"
@@ -25,37 +16,36 @@ struct Output;
 
 #[derive(Clone, Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Config {
+struct Configuration {
     value: Option<String>,
     excluded_variant_gids: Option<Vec<String>>,
 }
 
 #[derive(Clone, Debug, Deserialize)]
-struct Payload {
-    input: input::ResponseData,
-    configuration: Config,
+struct Input {
+    configuration: Configuration,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let payload: Payload = serde_json::from_reader(std::io::BufReader::new(std::io::stdin()))?;
+    let input: Input = serde_json::from_reader(std::io::BufReader::new(std::io::stdin()))?;
     let mut out = std::io::stdout();
     let mut serializer = serde_json::Serializer::new(&mut out);
-    script(payload)?.serialize(&mut serializer)?;
+    script(input)?.serialize(&mut serializer)?;
     Ok(())
 }
 
-fn script(payload: Payload) -> Result<output::ScriptOutput, Box<dyn std::error::Error>> {
+fn script(input: Input) -> Result<output::ScriptOutput, Box<dyn std::error::Error>> {
     const DEFAULT_VALUE: f64 = 50.0;
 
-    let (_input, config) = (payload.input, payload.configuration);
+    let config = input.configuration;
     let value: f64 = if let Some(value) = config.value {
         value.parse()?
     } else {
         DEFAULT_VALUE
     };
     let excluded_variant_gids = &config.excluded_variant_gids.unwrap_or_default();
-    let targets = vec![target(&excluded_variant_gids)];
-    return Ok(build_output(value, targets));
+    let targets = vec![target(excluded_variant_gids)];
+    Ok(build_output(value, targets))
 }
 
 fn target(excluded_variant_gids: &[String]) -> output::Target {
@@ -89,35 +79,15 @@ fn build_output(value: f64, targets: Vec<output::Target>) -> output::ScriptOutpu
 mod tests {
     use super::*;
 
-    fn payload(configuration: Config) -> Payload {
-        let input = r#"
-        {
-            "input": {
-                "merchandiseLines": [
-                    { "variant": { "id": 0 } },
-                    { "variant": { "id": 1 } }
-                ]
-            },
-            "configuration": {
-                "value": null,
-                "excludedVariantGids": null
-            }
-        }
-        "#;
-        let default_payload: Payload = serde_json::from_str(&input).unwrap();
-        Payload {
-            configuration,
-            ..default_payload
-        }
-    }
-
     #[test]
     fn test_discount_with_default_value() {
-        let payload = payload(Config {
-            value: None,
-            excluded_variant_gids: None,
-        });
-        let output = serde_json::json!(script(payload).unwrap());
+        let input = Input {
+            configuration: Configuration {
+                value: None,
+                excluded_variant_gids: None,
+            },
+        };
+        let output = serde_json::json!(script(input).unwrap());
 
         let expected_json = r#"
             {
@@ -143,11 +113,13 @@ mod tests {
 
     #[test]
     fn test_discount_with_value() {
-        let payload = payload(Config {
-            value: Some("10".to_string()),
-            excluded_variant_gids: None,
-        });
-        let output = serde_json::json!(script(payload).unwrap());
+        let input = Input {
+            configuration: Configuration {
+                value: Some("10".to_string()),
+                excluded_variant_gids: None,
+            },
+        };
+        let output = serde_json::json!(script(input).unwrap());
 
         let expected_json = r#"
             {
@@ -173,11 +145,13 @@ mod tests {
 
     #[test]
     fn test_discount_with_excluded_variant_gids() {
-        let payload = payload(Config {
-            value: None,
-            excluded_variant_gids: Some(vec!["gid://shopify/ProductVariant/0".to_string()]),
-        });
-        let output = serde_json::json!(script(payload).unwrap());
+        let input = Input {
+            configuration: Configuration {
+                value: None,
+                excluded_variant_gids: Some(vec!["gid://shopify/ProductVariant/0".to_string()]),
+            },
+        };
+        let output = serde_json::json!(script(input).unwrap());
 
         let expected_json = r#"
             {
